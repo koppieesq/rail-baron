6ab30c1275539b0e6d22e126c7090daeacbed5ab
Jordan Koplowicz <koppiejd@gmail.com>
2026-04-19 13:38:33 -0700

    messing with the nav bar - ii

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
 import GameMap from './GameMap';
 
+function AppContent() {
+  const location = useLocation();
+  const isMap = location.pathname === '/map';
+
+  return (
+    <div className={`app${isMap ? ' app--map' : ''}`}>
+      <header className={`app-header${isMap ? ' app-header--map' : ''}`}>
+        <Navbar />
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<GameMap />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="app">
-        <header className="app-header">
-          <Navbar />
-        </header>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/map" element={<GameMap />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
