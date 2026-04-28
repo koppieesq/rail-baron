4c9318c74bd5c38aa530a9a426a69e49bda89b4e
Jordan <jkoplowicz@tesla.com>
2026-04-28 07:54:00 -0700

    update About.js

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,18 +1,22 @@
-import { T } from './Translator';
+import {T} from './Translator';
 
 function About() {
-  return (
-    <div className="main-content">
-      <h2><T>About This Site</T></h2>
-      <p><T>Special thanks to <a href="https://www.avalonhill.com/worldwide">Avalon Hill</a> for many years of entertainment.</T></p>
-      <hr />
-      <h2><T>About Us</T></h2>
-      <p><T>My name is Jordan Koplowicz and I am the sole proprietor of Koplowicz & Sons.  I have been building websites since 2005, and writing software a lot longer than that.  Please see my portfolio for some examples of my work.</T></p>
-      <p><T>I work almost entirely with content management systems, especially Drupal.  I employ the latest techniques, including responsive design and SASS.</T></p>
-      <p><T>I live in San Francisco with my wife and two sons.</T></p>
-      <p><img style={{width: "100%"}} alt="Jordan Koplowicz and sons" src="Koplowicz-old.jpg"/></p>
-    </div>
-  );
+    return (<div className="main-content">
+            <h2><T>About This Site</T></h2>
+            <p><T>This is a <strong>technology demonstration</strong> to illustrate how to build a sophisticated,
+                modern site using headless Drupal, React, and <em>Kubernetes.</em>  This site was originally built for
+                <strong>Stanford Web Camp 2026</strong>; you are invited to read Jordan's
+                <a href="/presentation/demystifying-kubernetes.html">slide deck.</a></T></p>
+            <p><T>Special thanks to <a href="https://www.avalonhill.com/worldwide">Avalon Hill</a> for many years of
+                entertainment.</T></p>
+            <hr/>
+            <h2><T>About Us</T></h2>
+            <p><T>My name is Jordan Koplowicz and I am the sole proprietor of <a href="https://koplowiczandsons.com">
+                Koplowicz & Sons</a>. I have been building websites since 2005, and writing software a lot longer than
+                that.</T></p>
+            <p><T>I invite you to visit my professional site: https://koplowiczandsons.com</T></p>
+            <p><img style={{width: "100%"}} alt="Jordan Koplowicz and sons" src="Koplowicz-old.jpg"/></p>
+        </div>);
 }
 
 export default About;
