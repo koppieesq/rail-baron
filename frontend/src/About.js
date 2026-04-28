4b7e1cc5f4608a42355600146cd64e5df1548b29
Jordan <jkoplowicz@tesla.com>
2026-04-28 08:23:16 -0700

    update README.md

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -4,9 +4,10 @@ function About() {
     return (<div className="main-content">
             <h2><T>About This Site</T></h2>
             <p><T>This is a <strong>technology demonstration</strong> to illustrate how to build a sophisticated,
-                modern site using headless Drupal, React, and <em>Kubernetes.</em>  This site was originally built for
-                <strong>Stanford Web Camp 2026</strong>; you are invited to read Jordan's
-                <a href="/presentation/demystifying-kubernetes.html">slide deck.</a></T></p>
+                modern site using headless Drupal, React, and <strong>Kubernetes.</strong>  This site was originally built
+                for <strong>Stanford Web Camp 2026</strong>; you are invited to read
+                Jordan's <a href="/presentation/demystifying-kubernetes.html">slide deck</a>.  You can also view the
+                original <a href="https://webcamp.stanford.edu/session/kubernetes-for-drupal-devs">session description</a>.</T></p>
             <p><T>Special thanks to <a href="https://www.avalonhill.com/worldwide">Avalon Hill</a> for many years of
                 entertainment.</T></p>
             <hr/>
@@ -14,7 +15,7 @@ function About() {
             <p><T>My name is Jordan Koplowicz and I am the sole proprietor of <a href="https://koplowiczandsons.com">
                 Koplowicz & Sons</a>. I have been building websites since 2005, and writing software a lot longer than
                 that.</T></p>
-            <p><T>I invite you to visit my professional site: https://koplowiczandsons.com</T></p>
+            <p><T>I invite you to visit my professional site: <a href="https://koplowiczandsons.com">Koplowicz & Sons</a>.</T></p>
             <p><img style={{width: "100%"}} alt="Jordan Koplowicz and sons" src="Koplowicz-old.jpg"/></p>
         </div>);
 }
